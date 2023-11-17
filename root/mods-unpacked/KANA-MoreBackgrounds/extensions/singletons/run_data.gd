c72b09426f7c6e16c7ccbfaad90285ec6f4fe76a
Kai <kai@kana.jetzt>
2023-11-17 21:29:01 +0100

    fix: :bug: fix RunData reset issue
    Resolves an issue caused by the RunData being reset on game start.

--- root/mods-unpacked/KANA-MoreBackgrounds/extensions/singletons/run_data.gd
+++ root/mods-unpacked/KANA-MoreBackgrounds/extensions/singletons/run_data.gd
@@ -3,7 +3,19 @@ extends "res://singletons/run_data.gd"
 
 const KANA_MOREBACKGROUNDS_RUN_DATA_LOG_NAME := "KANA-MoreBackgrounds:RunData"
 
-func _ready() -> void:
+var is_custom_background_appended := false
+
+
+func reset_background() -> void:
+	KANA_add_custom_backgrounds()
+	.reset_background()
+
+
+func KANA_add_custom_backgrounds() -> void:
+	if is_custom_background_appended:
+		return
+
 	var all_custom_backgrounds: Array = load("res://mods-unpacked/KANA-MoreBackgrounds/content/backgrounds/all_backgrounds.tres").backgrounds
 	backgrounds.append_array(all_custom_backgrounds)
 	ModLoaderLog.info("Added %s custom backgrounds." % all_custom_backgrounds.size(), KANA_MOREBACKGROUNDS_RUN_DATA_LOG_NAME)
+	is_custom_background_appended = true
